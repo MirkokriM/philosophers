78d534fdc0a0b5851d5e4d97101065e420dc4768
FirestormPenguin <edoardogiubellini@gmail.com>
2023-07-21 19:09:01 +0200

    commit

--- philospers.h
+++ philospers.h
@@ -1,18 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philosopers.c                                      :+:      :+:    :+:   */
+/*   philospers.h                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: egiubell <egiubell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/21 18:34:05 by egiubell          #+#    #+#             */
-/*   Updated: 2023/07/21 18:37:36 by egiubell         ###   ########.fr       */
+/*   Created: 2023/07/21 18:34:41 by egiubell          #+#    #+#             */
+/*   Updated: 2023/07/21 18:37:32 by egiubell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philosopers.h"
+#ifndef PHILOSPERS_H
+# define PHILOSPERS_H
 
-int main()
-{
-	
-}
+# include <unistd.h>
+# include <stdio.h>
+
+
+
+#endif
